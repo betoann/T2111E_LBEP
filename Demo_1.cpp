d084a069e6593c515ca3df1fd7fea94021a4e23a
betoann <toandgkthd12@gmail.com>
2021-12-02 18:10:37 +0700

    Demo code

---
+++ Demo_1.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(){
+	
+	int A,B;
+	printf("Nhap A= ");
+	scanf("%d" ,&A);
+	printf("Nhap B= ");
+	scanf("%d", &B);
+	
+	if (A>B) {
+		printf("So lon hon: %d",A);
+	}
+	else {
+		printf("So lon hon: %d",B);
+	}
+}
