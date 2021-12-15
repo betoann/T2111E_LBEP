073b3f22eefeffc49759fe81dfca8ab6a325864c
betoann <toandgkthd12@gmail.com>
2021-12-15 21:30:53 +0700

    BT labs7

---
+++ Labs7/BT6.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(){
+	int n;
+	printf("Nhap n= ");
+	scanf("%d",&n);
+	int arr[n];
+	for (int i=0; i<n; i++){
+		printf("Nhap phan tu arr[%d]= ",i);
+		scanf("%d",&arr[i]);
+	}
+	int max=0,count=0;
+	for (int i=0; i<n; i++){
+		if (arr[i] > 0){
+			count++;
+		}else{
+			if (max < count){
+				max=count;
+				count=0;
+			}
+		}
+	}
+	if (max > 0){
+		printf("So luong cac so duong lien tiep nhieu nhat la: %d",max);
+	}else{
+		printf("Mang khong co so duong!");
+	}
+}
